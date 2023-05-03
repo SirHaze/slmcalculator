d72182e4c86601c5ed11974d6c1e1eb4ae747111
SirHaze <114669880+SirHaze@users.noreply.github.com>
2023-05-03 10:28:31 +0200

    initial commit

---
+++ src/test/java/com/example/slmcalculator/SlmcalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slmcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmcalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
